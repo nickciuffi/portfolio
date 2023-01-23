a0e357956e5ea6be8fe5fda1f2871ed5b00cd7a2
nicolas ciuffi <nicolasciuffi1@gmail.com>
2023-01-23 20:47:52 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="container">
+      <div className='name-shower'>
+      <p className="pre name-comp">Olá, meu nome é</p>
+      <div className="name-cont">
+      <p className='name'>NICOLAS CIUFFI</p>
+      </div>    
+      <p className='pos name-comp'>I`m a Web Developer</p>  
+      </div>
+    </div>
+  )
+}
+
+export default App
